eec8ed642efea9d2cf72bacd1c1d917da645b95b
ghostghost664 <66411750+ghostghost664@users.noreply.github.com>
2020-06-08 15:24:01 +0700

    Add files via upload

---
+++ Exercise4_Chinnawat_P.py
@@ -0,0 +1,14 @@
+x = 99.9
+
+y = 10.2
+
+z = 60
+
+v = 70
+
+print("--- Your Score ---")
+
+print("Foundation English",x ,"คะแนน")
+print("General Business",y ,"คะแนน")
+print("Introduction to Computer Systems",z ,"คะแนน")
+print("Computer Programming",v ,"คะแนน")
